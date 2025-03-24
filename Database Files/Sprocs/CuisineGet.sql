45d8979a0f3ce2d476c0520fffca8b47f23df0e5
chanamindy <chanamindyh@gmail.com>
2025-03-23 20:20:38 -0400

    HW #17 Recipe Sprocs

---
+++ Database Files/Sprocs/CuisineGet.sql
@@ -0,0 +1,29 @@
+use RecipeWebsiteDB
+go 
+
+create or alter procedure dbo.CuisineGet(@CuisineId int = 0, @All bit = 0, @CuisineType varchar(35) = '')
+as
+begin
+	select @CuisineType = NullIf(@CuisineType, '')
+	select c.CuisineId, c.CuisineType
+	from Cuisine c
+	where c.CuisineId = @CuisineId
+	or @All = 1
+	or c.CuisineType like '%' + @CuisineType + '%'
+	order by c.CuisineType
+end
+go
+
+/*
+execute dbo.CuisineGet --return no results
+
+execute dbo.CuisineGet @CuisineType = null --return no results
+
+declare @CuisineId int
+select top 1 @CuisineId = CuisineId from Cuisine
+execute dbo.CuisineGet @CuisineId = @CuisineId
+
+execute dbo.CuisineGet @All = 1
+
+execute dbo.CuisineGet @CuisineType = 'a'
+*/
