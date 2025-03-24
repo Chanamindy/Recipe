3e9c6d2a314c27610e432e090eab4beaafd58b11
chanamindy <chanamindyh@gmail.com>
2025-03-23 20:14:42 -0400

    Committed with correct db connection

--- RecipeApps/RecipeWinForms/Program.cs
+++ RecipeApps/RecipeWinForms/Program.cs
@@ -14,8 +14,7 @@ namespace RecipeWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            DBManager.SetConnectionString("Server=.\\SQLExpress;Database=RecipeWebsiteDB;Trusted_Connection=true");
-            //"Server=tcp:cmherszaft.database.windows.net,1433;Initial Catalog=RecipeWebsiteDB;Persist Security Info=False;User ID=Chanacpuadmin;Password=Chanaadmin412;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            DBManager.SetConnectionString("Server=tcp:cmherszaft.database.windows.net,1433;Initial Catalog=RecipeWebsiteDB;Persist Security Info=False;User ID=Chanacpuadmin;Password=Chanaadmin412;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             Application.Run(new frmRecipeSearch());
         }
     }
